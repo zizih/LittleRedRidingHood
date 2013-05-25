c31954486647c8296e4b1b371b8ac43e31c054f6
zizihjk <zizihjk@gmail.com>
2013-05-25 17:55:24 +0800

    remove auto

--- src/andr/lexibook/mylittlestory/lrrh/ui/Pages.java
+++ src/andr/lexibook/mylittlestory/lrrh/ui/Pages.java
@@ -37,7 +37,6 @@ public class Pages extends BaseActivity {
         flipView.setAdapter(flipAdapter);
         setContentView(flipView);
 
-        setting.setAuto(false);
         flipListener = new Fliplistener();
         flipView.setFlipByTouchEnabled(true);
         flipView.setOnViewFlipListener(flipListener);
